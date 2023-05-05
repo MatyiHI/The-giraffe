f8d392e2174bebeae61d079267352a4240f58f99
Kreidler, Matyas Edgar <88744380+MatyiHI@users.noreply.github.com>
2023-05-05 09:56:23 -1000

    Added Giraffe Image

---
+++ images.g.ts
@@ -0,0 +1,44 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "Giraffe":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . 5 . . . . . . . . 
+. . . . . e 5 5 e . . . . . . . 
+. . . 5 5 5 5 e e e . . . . . . 
+. . 5 5 f 5 5 5 e . . . . . . . 
+. . 5 5 5 3 5 5 e e . . . . . . 
+. . . 5 5 5 5 5 e . . . . . . . 
+. . . . . 5 5 f e e . . . . . . 
+. . . . . 5 5 5 e . . . . . . . 
+. . . . . f 5 5 e e . . . . . . 
+. . . . . 5 5 5 e . . e e . . . 
+. . . . . f 5 5 e e 5 e . . . . 
+. . . . . 5 5 5 5 5 5 . . . . . 
+. . . . . 5 f 5 5 f 5 . . . . . 
+. . . . . 5 5 . . 5 5 . . . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
